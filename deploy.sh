8ab1c937b042b71088f18b3a5ed15fd1de8a15cf
Mark Price <mark@aitusoftware.com>
2019-03-21 07:53:09 +0000

    Add xmonad to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,8 @@ DIR=$(dirname "$0")
 cp "$DIR/.bash_aliases" $HOME/
 cp "$DIR/.tmux.conf" $HOME/
 cp "$DIR/.vimrc" $HOME/
+mkdir -p $HOME/.xmonad
+cp xmonad.hs $HOME/.xmonad
+cp .xmobarrc $HOME/
 source "$HOME/.bash_aliases"
+
